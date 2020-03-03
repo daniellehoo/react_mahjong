d221d1d35efd39223c12a79cf5650fc6a6a7bc56
Danielle Hoo <daniellem.hoo@gmail.com>
2020-03-03 15:06:09 -0500

    updated tile selections

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import one from "./images/1.png";
 import two from "./images/2.png";
@@ -35,8 +35,32 @@ function App() {
 }
 
 function Main() {
+  const [board, setBoard] = useState("one two three");
+  const [selected, setSelected] = useState([]);
   return (
     <div>
+      <input
+        type="text"
+        value={board}
+        onChange={e => {
+          setBoard(e.target.value);
+          while (selected.length < board.split(" ").length)
+            selected.push(false);
+        }}
+      />
+      {board.split(" ").map((id, index) => {
+        return (
+          <Card
+            id={id}
+            selected={selected[index]}
+            callBack={() => {
+              selected[index] = !selected[index]
+              setSelected(selected.slice());
+            }}
+          />
+        );
+      })}
+
       <ul>
         {" "}
         {cards.map(card => {
@@ -50,6 +74,20 @@ function Main() {
     </div>
   );
 }
+
+function Card({ id, callBack, selected }) {
+  let card = cards.find(item => item.id == id);
+  return card ? (
+    <img
+      src={card.image}
+      onClick={callBack}
+      style={selected ? { border: "1px solid red" } : {}}
+    />
+  ) : (
+    <div></div>
+  );
+}
+
 function Child() {
   // We can use the `useParams` hook here to access
   // the dynamic pieces of the URL.
